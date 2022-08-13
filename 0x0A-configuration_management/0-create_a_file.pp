ca9b5b5d0cb3b965f373d3108d939ebbe4b2c498
yousseffabid <abidyoussef58@gmail.com>
2022-08-13 08:06:02 +0100

    pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 #create file in /tmp
 
-file { 'school':
-    ensure      => present,
-    mode        => '0744',
-    content     => 'I love Puppet',
-    path        => '/tmp/school',
-    owner       => 'www-data',
-    group       => 'www-data'
+file { '/tmp/school':
+    ensure  => present,
+    mode    => '0744',
+    content => 'I love Puppet',
+    owner   => 'www-data',
+    group   => 'www-data'
 }
