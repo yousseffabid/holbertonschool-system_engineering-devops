614a85a51df657efb39c847a16c8541883687cb4
yousseffabid <abidyoussef58@gmail.com>
2022-09-27 14:39:17 +0100

    fixed

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,18 +9,18 @@ def recurse(subreddit, hot_list=[], after=''):
     if not subreddit:
         return None
     url = "https://www.reddit.com"
-    url_endpoint = "/r/{}/hot.json?limit=100&after={}".format(subreddit, after)
+    endpoint = "/r/{}/hot.json?limit=100&after={}".format(subreddit, after)
 
-    req = requests.get("{}{}".format(url, url_endpoint),
+    request = requests.get("{}{}".format(url, endpoint),
                            headers={'User-agent': 'Mozilla/5.0'},
                            allow_redirects=False)
 
-    if req.status_code == 200:
+    if request.status_code == 200:
         children = request.json().get('data').get('children')
         after = request.json().get('data').get('after')
 
-        for el in children:
-            hot_list.append(el.get('data').get('title'))
+        for elem in children:
+            hot_list.append(elem.get('data').get('title'))
 
         if after is not None:
             recurse(subreddit, hot_list, after)
